ceaae3117306e104fc79f2957507703ea780ab36
Mark Dijkstra <markdijkstra.eu@gmail.com>
2021-04-27 08:05:50 +0200

    cleanup code

--- index.php
+++ index.php
@@ -4,19 +4,12 @@
     <title>Chessboard</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
-<body>
-      
+<body>      
     <div class="container">
         <?php 
             include 'classes/Chessboard.php';
             $chessboard = new Chessboard;
             $chessboard->buildBoard();
-
-            echo '<pre>';
-            print_r($chessboard);
-            echo '</pre>';
-
         ?>
     </div>
 </body>
